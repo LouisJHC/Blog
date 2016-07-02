673e44c90d188c8b8904b9d3617bcb96f8de4ea6
louisjhc <lcha@sfu.ca>
2016-07-02 22:42:19 +0000

    ability to add, delete post + page controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
     validates :title, presence: true, length: { minimum: 2 }
     validates :body, presence: true, length: { maximum: 350 }
-    has_many :comments
+    has_many :comments, dependent: :destroy
 end
